016a17ee5a55ff516e0390e101b2cb0e9c2eda53
Jarvyes14 <javierbarcelosantos@gmail.com>
2025-01-17 11:34:25 -0600

    Matadero

--- POO_con_python.py
+++ POO_con_python.py
@@ -1,3 +1,5 @@
+import os
+
 class Personaje:
 
     nombre = "Default"
@@ -47,9 +49,71 @@ class Personaje:
         return "Soy un personaje"
     
 class Guerrero(Personaje):
-    pass
 
-tlatoani = Guerrero("Tlatoani", 10, 100, 5, 20)
+    def __init__(self, nombre, fuerza, vida, defensa, inteligencia, espada):
+        super().__init__(nombre, fuerza, vida, defensa, inteligencia)
+        self.espada = espada
+
+    def imprimir_atributos(self):
+        super().imprimir_atributos
+        print(f"Espada: {self.espada}")
+
+    def elegir_arma(self):
+        opcion = input("Elige un arma: \n(1)Lanza, daño 10 \n(2)Espada, daño 15 \n(3)Arco, daño 20 \n>")
+        if opcion == "1":
+            self.espada = 10
+        elif opcion == "2":
+            self.espada = 15
+        elif opcion == "3":
+            self.espada = 20
+        else:
+            print("Opción no válida \n")
+            input("Presiona enter para continuar \n")
+            os.system('cls')
+            self.elegir_arma()
+    
+    def daño(self, enemigo):
+        daño = self.fuerza * self.espada - enemigo.defensa
+        enemigo.vida -= daño
+        if enemigo.vida <= 0:
+            enemigo.morir()
+        return f"El enemigo ha recibido {daño} puntos de daño"
+    
+class Mago(Personaje):
+
+    def __init__(self, nombre, fuerza, vida, defensa, inteligencia, libro):
+        super().__init__(nombre, fuerza, vida, defensa, inteligencia)
+        self.libro = libro
+
+    def imprimir_atributos(self):
+        super().imprimir_atributos
+        print(f"Espada: {self.libro}")
+
+    def elegir_arma(self):
+        opcion = input("Elige un arma: \n(1)Libro de hechizos, daño 10 \n(2)Baculo, daño 15 \n(3)Varita, daño 20 \n>")
+        if opcion == "1":
+            self.libro = 10
+        elif opcion == "2":
+            self.libro = 15
+        elif opcion == "3":
+            self.libro = 20
+        else:
+            print("Opción no válida \n")
+            input("Presiona enter para continuar \n")
+            os.system('cls')
+            self.elegir_arma()
+    
+    def daño(self, enemigo):
+        daño = self.inteligencia * self.libro - enemigo.defensa
+        enemigo.vida -= daño
+        if enemigo.vida <= 0:
+            enemigo.morir()
+        return f"El enemigo ha recibido {daño} puntos de daño"
+
+
+tlatoani = Guerrero("Tlatoani", 10, 100, 5, 20, 5)
+tlatoani.elegir_arma()
+tlatoani.imprimir_atributos()
 
 mi_personaje = Personaje("Gandalf", 10, 100, 5, 20)
 mi_personaje.imprimir_atributos
